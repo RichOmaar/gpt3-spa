2269b90dc0f66e118219624462c3018d1bae5a9d
RichOmaar <39716286+RichOmaar@users.noreply.github.com>
2023-12-10 23:51:34 -0600

    Latest updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
+import Brands from './containers/brands/Brands';
+import Header from './containers/header/Header';
+import WhatGPT3 from './containers/whatGPT3/WhatGPT3';
 
 function App() {
   return (
     <div className="App">
       <div className="gradient__bg">
         <Navbar />
+        <Header />
       </div>
+      <Brands />
+      <WhatGPT3 />
     </div>
   );
 }
